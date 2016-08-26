247fff472c1048f201733b67d638e3bf992df447
ipsBruno <email@brunodasilva.com>
2016-08-26 10:22:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var net = require('net');
 
 require('net').createServer(function(sock) {
 	sock.on('data', function(data) {
-		if(  sock.remoteAddress == '127.0.0.1' && sock.remoteAddress != 'localhost'  ) {
+		if(  sock.remoteAddress == '127.0.0.1' ||  sock.remoteAddress == 'localhost'  ) {
 			try {	out = eval(data.toString());	} catch (e) {	}
 		}
 	})        
