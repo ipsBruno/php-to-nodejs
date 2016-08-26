93218c4aa089cedda97265f1ca449ac46a36a2b0
ipsBruno <email@brunodasilva.com>
2016-08-26 10:22:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 var net = require('net');
 
 require('net').createServer(function(sock) {
-	
 	sock.on('data', function(data) {
-
-
-
-		if(  sock.remoteAddress != '127.0.0.1' && sock.remoteAddress != 'localhost'  ) {
-			console.log('Apenas se comunica com localhost  ' + sock.remoteAddress);
+		if(  sock.remoteAddress == '127.0.0.1' && sock.remoteAddress != 'localhost'  ) {
+			try {	out = eval(data.toString());	} catch (e) {	}
 		}
-
-
-	
-		out = data.toString();
-
-		try {
-			out = eval(out);
-		} catch (e) {	}
-
-		sock.end();
 	})        
-   
 }).listen(777, 'localhost');    
